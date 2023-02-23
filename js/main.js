84b6866465249f1521cfccfc0a804f8b8e6389b0
lancymjr <lancymjr@gmail.com>
2023-02-22 21:44:10 -0500

    added css

--- js/main.js
+++ js/main.js
@@ -15,25 +15,34 @@ form.addEventListener("submit", function (e) {
 function task(input) {
   // create new list-item
   const li = document.createElement("li");
+  li.classList.add("list-item");
 
   // create a new input for list item
   const li_Input = document.createElement("input");
+  li_Input.classList.add("li-Input");
   li_Input.type = "text";
   li_Input.value = input;
   li_Input.setAttribute("readonly", "readonly");
 
+  // create container for buttons
+  const btnContainer = document.createElement("div");
+  btnContainer.classList.add("btnContainer");
+
   // create edit button
   const editBtn = document.createElement("button");
   editBtn.setAttribute("id", "edit");
+  editBtn.classList.add("editBtn");
   editBtn.innerText = "Edit";
   // function of edit button.
   editBtn.addEventListener("click", function () {
     if (editBtn.innerText === "Edit") {
       li_Input.removeAttribute("readonly");
+      li_Input.classList.add("edit-li-Input-Color");
       li_Input.focus();
       editBtn.innerText = "Save";
     } else {
       li_Input.setAttribute("readonly", "readonly");
+      li_Input.classList.remove("edit-li-Input-Color");
       editBtn.innerText = "Edit";
     }
   });
@@ -41,15 +50,20 @@ function task(input) {
   // create delete button
   const deleteBtn = document.createElement("button");
   deleteBtn.setAttribute("id", "delete");
+  deleteBtn.classList.add("deleteBtn");
   deleteBtn.innerText = "Delete";
-  // function of delete button. Deletes the parent element on click
+  // function of delete button. Plays animation, waits 2 seconds, then deletes the grandparent element on click
   deleteBtn.addEventListener("click", function () {
-    deleteBtn.parentElement.remove();
+    li.classList.add("deleteBtn-Animation");
+    setTimeout(function () {
+      deleteBtn.parentElement.parentElement.remove();
+    }, 1500);
   });
 
-  // appends edit and delete button to list-item then appends list-item to unordered list
+  // appends edit and delete button to btnContainer, appends li-input & btnContainer to list-item, then appends list-item to unordered list
+  btnContainer.append(editBtn);
+  btnContainer.append(deleteBtn);
   li.append(li_Input);
-  li.append(editBtn);
-  li.append(deleteBtn);
+  li.append(btnContainer);
   ul.append(li);
 }
